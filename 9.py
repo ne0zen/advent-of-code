701f7276a35669a9eb40c85e8931567e56d0bbc0
Ross Heflin <heflin.rosst@gmail.com>
2021-02-07 12:40:39 -0600

    day9: use deque instead

--- 9.py
+++ 9.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
 
+import collections
 import itertools
 
+
 def find_xmas_vuln(data, preamble_size=25):
-    window = list(int(v) for v in data[:preamble_size])
+    window = collections.deque(
+        int(v) for v in data[:preamble_size]
+    )
 
     for v in data[preamble_size:]:
         v = int(v)
-        found = False or len(window) < 2
+        found = len(window) < 2 or False
         for i1, i2 in itertools.permutations(window, 2):
             if i1 + i2 == v:
                 found = True
@@ -15,26 +19,22 @@ def find_xmas_vuln(data, preamble_size=25):
         if found:
             window.append(v)
             if len(window) > preamble_size:
-                window.pop(0)
+                window.popleft()
         else:
             return v
+
 
 def find_int_range(raw_data, preamble_size=25):
     vuln = find_xmas_vuln(raw_data, preamble_size)
     data = [int(v) for v in raw_data]
 
-    i = 0
-    while i < len(data):
-        size = 0
-        while True:
-            size += 1
-            stuff = data[i:i+size]
+    for i in range(len(data)):
+        for j in range(i + 1, len(data)):
+            stuff = data[i:j]
             if (sum_stuff := sum(stuff)) == vuln:
                 return stuff
             elif sum_stuff > vuln:
                 break
-        i += 1
-
 
 
 if __name__ == "__main__":
